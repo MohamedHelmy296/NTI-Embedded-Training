6856ded537c063f8a5c57de85027a515a823e2ff
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-11-08 22:22:15 +0200

    Create program_2.c

---
+++ C and Embedded C/session_1/program_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("This program to covert char to long interger \n ");
+    char c ;
+    printf("Enter the char :");
+    scanf(" %c", &c);
+    printf("the intger of char %c is %i",c,c);
+  
+    return 0 ;
+}
