9dc62d68349e8c3353e35ef20a79e99fd1e1e3b1
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-11-08 22:23:55 +0200

    Create program_3.c

---
+++ C and Embedded C/session_1/program_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    printf("This program to make swap between to number \n");
+    int number_one ,number_two ,temp ;
+    printf("please enter number_one :");
+    scanf(" %i", &number_one);
+    printf("please enter number_two :");
+    scanf(" %i", &number_two);
+    printf("Befor swaping number_one is : %i , number_two is : %i \n",number_one , number_two);
+    temp = number_two ;
+    number_two = number_one ;
+    number_one = temp ;
+    printf("After swaping number_one is : %i , number_two is : %i \n",number_one , number_two);
+
+    return 0 ;
+}
