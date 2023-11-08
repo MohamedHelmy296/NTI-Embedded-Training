7a8b40d33ad20c1a867a34d8e471c1351d9299ed
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-11-08 22:46:41 +0200

    Create program_4.c

---
+++ C and Embedded C/session_2/program_4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main ()
+{
+    printf("Program for analysis of people of certain age groups with salary "
+           "who are eligible for getting a suitable loan in banking system  if "
+           "their condition and norms get satisfied using nested if statement."
+           "Age<18 illegal (>18,<50 for salary >3000) accept--->50 illegal"
+           "Show details for user in weakness point \n \n");
+
+    int age ;
+    float salary ;
+    printf("please enter your age: \n");
+    scanf(" %i", &age);
+    if( age > 18)
+    {
+        if (age < 50)
+        {
+            printf("enter your salary : \n");
+            scanf(" %f", &salary);
+            if (salary > 3000)
+            {
+                printf("your are accpeted \n");
+            }
+            else
+            {
+                printf("your salary less than 3000 you can't loan \n");
+            }
+        }
+        else
+        {
+            printf("your age is bigger than 50 you can't pay your loan \n");
+
+        }
+    }
+    else
+    {
+        printf("your age is less than 18 so you are illegal \n");
+    }
+    return 0 ;
+}
