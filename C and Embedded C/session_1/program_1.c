933554add3e17f4750a36092828d24c075ac042e
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-11-08 22:24:05 +0200

    Update and rename here.c to program_1.c

---
+++ C and Embedded C/session_1/program_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    printf("This program to enter id, score of 2 student  and Display them and sum of score \n ");
+    int id_1;
+    int id_2;
+    float score_1;
+    float score_2;
+    float sum = 0 ;
+    int no = 1 ;
+    printf("Please Enter the data of student %i :",no);
+    scanf("%i", &id_1);
+    scanf("%f", &score_1);
+    no++;
+    printf("Please Enter the data of student %i : ",no);
+    scanf("%i", &id_2);
+    scanf("%f", &score_2);
+    printf("The id_1 : %i and the score : %f \n", id_1, score_1);
+    printf("The id_2 : %i and the score : %f \n", id_2, score_2);
+    sum = score_1 + score_2;
+    printf(" \n The sum of score : %f \n",sum);
+
+    return 0 ;
+}
