ba4ffde571a91d676c8642dbedf7cf43a3e701d3
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-11-08 22:52:33 +0200

    Create program_5.c

---
+++ C and Embedded C/session_2/program_5.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+int main ()
+{
+    /* calculate program */
+    char operator_;
+    int num_1, num_2, result;
+    printf("please enter your operator : \n");
+    scanf(" %c", &operator_ );
+    printf("please enter two numbers : \n");
+    scanf(" %i %i", &num_1, &num_2);
+    switch(operator_)
+    {
+    case '+':
+    {
+        result = num_1 + num_2;
+        printf("%i %c %i = %i", num_1,operator_, num_2, result );
+    }
+    break ;
+
+    case '-':
+    {
+        if (num_1 > num_2)
+        {
+            result = num_1 - num_2;
+            printf("%i %c %i = %i", num_1,operator_, num_2, result );
+        }
+        else
+        {
+            result = num_2 - num_1;
+            printf("%i %c %i = %i", num_2,operator_, num_1, result );
+        }
+    }
+    break ;
+
+    case '*':
+    {
+        if ((num_1&&num_2) == 1)
+        {
+            result = num_1 * num_2;
+            printf("%i %c %i = %i", num_1,operator_, num_2, result );
+        }
+        else
+        {
+            printf(" invalid number \n");
+        }
+
+    }
+    break ;
+    case '/':
+    {
+        if ( num_1 > num_2)
+        {
+            if ( num_2 == 0 )
+            {
+                printf("error \n");
+            }
+            else
+            {
+                result = num_1 / num_2;
+                printf("%i %c %i = %i", num_1,operator_, num_2, result );
+            }
+
+        }
+        else
+        {
+            if ( num_1 == 0 )
+            {
+                printf("error \n");
+            }
+            else
+            {
+                result = num_2 / num_1;
+                printf("%i %c %i = %i", num_2,operator_, num_1, result );
+            }
+        }
+    }
+    break ;
+    case '%':
+    {
+        if ( num_1 > num_2)
+        {
+            if ( num_2 == 0 )
+            {
+                printf("error \n");
+            }
+            else
+            {
+                result = num_1 % num_2;
+                printf("%i %c %i = %i", num_1,operator_, num_2, result );
+            }
+
+        }
+        else
+        {
+            if ( num_1 == 0 )
+            {
+                printf("error \n");
+            }
+            else
+            {
+                result = num_2 % num_1;
+                printf("%i %c %i = %i", num_2,operator_, num_1, result );
+            }
+        }
+    }
+    break ;
+
+    default:
+    {
+        printf("invalid operation \n");
+    }
+    }
+    return 0 ;
+}
