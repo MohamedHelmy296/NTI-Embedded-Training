e1a5ac38213712e40bb9e42e44ddf0ca1040a21f
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-11-08 22:37:59 +0200

    Create program_2.c

---
+++ C and Embedded C/session_2/program_2.c
@@ -0,0 +1,37 @@
+    #include <stdio.h>
+    int main ()
+{
+    float score;
+    int i ;
+    printf("This program to print the states of score \n");
+    printf("please enter your score : \n");
+    for(i = 0 ; i<100 ; i++)
+    {
+        scanf(" %f", &score );
+        if((score < 0 ) || (score > 100))
+        {
+            printf("error \n");
+        }
+        else if (score < 50)
+        {
+            printf("fail \n");
+        }
+        else if ((score >= 50) && (score < 65))
+        {
+            printf("pass \n");
+        }
+        else if ((score >= 65 )&& (score < 75))
+        {
+            printf("good \n");
+        }
+        else if ((score >= 75) && (score < 85 ))
+        {
+            printf("very good \n");
+        }
+        else if ((score >= 85 )&& (score <= 100))
+        {
+            printf("Ex \n");
+        }
+    }
+    return 0 ;
+    }
