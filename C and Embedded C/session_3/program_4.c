b899dbffc246bb687a824a0b667bd81c77ece634
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-11-09 10:54:57 +0200

    Create program_4.c

---
+++ C and Embedded C/session_3/program_4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    /*
+    program to print charater form a to z
+    */
+    char counter;
+    for(counter = 'a' ; counter <= 'z' ; counter++)
+    {
+        printf("%c \n", counter);
+    }
+
+
+    return 0 ;
+}
