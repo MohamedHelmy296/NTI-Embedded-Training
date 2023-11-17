d87475850194152315314a25b69f74414074cc81
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-11-17 10:59:37 +0200

    Update program_1.c

--- C and Embedded C/session_6/program_1.c
+++ C and Embedded C/session_6/program_1.c
@@ -1,8 +1,25 @@
+/*
+    function ------> use function to compare between two numbers    
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
+int compare (int y, int x );
+
 int main()
 {
-    printf("Hello world!\n");
+    int num_1, num_2  ;
+    printf("Please enter two number : \n");
+    scanf("%d %d",&num_1, &num_2);
+    comper(num_1,num_2);
+
     return 0;
+}
+
+int compare (int n1, int n2 )
+{
+    if (n1>n2)
+        printf("num_one %i > num_two %i",n1, n2);
+    else
+        printf("num_two %i > num_one %i",n2, n1);
 }
