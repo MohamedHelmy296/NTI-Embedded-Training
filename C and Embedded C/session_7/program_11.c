57bb21d21a1f96e03ed7d004e07606f01be9a0a1
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-11-29 09:35:55 +0200

    Create program_11.c

---
+++ C and Embedded C/session_7/program_11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+        int a = 5 ;
+        int reslut = sum(a);
+        printf("the result = %i \n", reslut);
+
+
+    return 0 ;
+}
+int sum (int x)
+{
+    if(x>0)
+        return x+sum(x-1) ;
+    else
+    return x ;
+}
